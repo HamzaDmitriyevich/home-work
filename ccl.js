f06a3c85be9d4a6d9503ee973cde0d2ceea7c8e4
HamzaDmitriyevich <krou1937@gmail.com>
2024-01-08 20:40:24 +0300

    Ошибки в консоли нет.Но не работает

--- ccl.js
+++ ccl.js
@@ -1,8 +1,14 @@
-function animalYear(){
+function animalsYear(){
     var year = year;
     
     var animals = ["Обезьяна ", "Петух" , "Собака" , "Свинья" , "Крыса" , "Бык" , "Тигр"
 , "Кролик" , "Дракон" , "Змея" , "Лошадь" , "Овца"];
+
+var anumal = animals[(year - 4) % 12];
+
+var image = "<img src='" + animal + ".jpg>";
+
+document.getElementById("animal").innerHTML = image;
 
 
 }
